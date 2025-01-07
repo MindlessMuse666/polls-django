a00f2972f233e019a89a7436d67c1727a623e03c
MindlessMuse666 <mindlessmuse.666@gmail.com>
2025-01-07 17:38:43 +0300

    settings, feat `polls`, database
    settings (`polls_site\settings.py`):
    1. Изменён часовой пояс приложения (`TIME_ZONE`) на Европа/Москва.
    2. Приложение зарегистрировано в `INSTALLED_APPS` более корректно.
    
    feat `polls`
    1. Создана и зарегистрирована в админке модель `Question`.
    2. Добавлена модель `Choice`.
    
    database:
    Выполнена миграция созданных моделей в БД SQLite.
    ---
    settings (`polls_site\settings.py `):
    1. The application's time zone (`TIME_ZONE') has been changed to Europe/Moscow.
    2. The application is registered in `INSTALLED_APPS` more correctly.
    
    feat `polls`
    1. The `Question` model has been created and registered in the admin panel.
    2. The `Choice` model has been added.
    
    database:
    The migration of the created models to the SQLite database has been completed.

--- polls_site/polls/models.py
+++ polls_site/polls/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.db.models import (Model, ForeignKey, CASCADE,
+                              CharField, DateTimeField, IntegerField)
+from django.utils.timezone import now, timedelta
 
-# Create your models here.
+
+class Question(Model):
+  question_text = CharField(max_length=255)
+  publish_datetime = DateTimeField('date published')
+
+  
+  def __str__(self):
+    return self.question_text
+  
+  
+  def was_published_recently(self):
+    return self.publish_datetime >= now() - timedelta(days=1)
+
+
+class Choice(Model):
+  question = ForeignKey(
+    Question,
+    on_delete=CASCADE
+  )
+  choice_text = CharField(max_length=255)
+  votes = IntegerField(default=0)
+  
+  
+  def __str__(self):
+      return self.choice_text
